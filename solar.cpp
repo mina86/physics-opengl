dcf563650c33d03eefa85f8ad5ba3fbb9eeba3d0
Michal Nazarewicz <mina86/AT/mina86.com>
2008-10-15 09:16:58 +0200

    Added cursor, fps counter and head light (which does not work)

--- solar.cpp
+++ solar.cpp
@@ -32,7 +32,7 @@ static void handleResize(int w, int h) {
 	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(45.0, (double)w / (double)h, 1.0, 10000.0);
+	gluPerspective(45.0, (double)w / (double)h, 0.1, 10000.0);
 }
 
 
@@ -44,28 +44,43 @@ private:
 };
 
 
+static unsigned fps_counter = 0;
+static float fps = 0;
+static void zeroFPS(int param) {
+	fps = fps * 0.25 + fps_counter * 0.75;
+	fps_counter = 0;
+	glutPostRedisplay();
+	glutTimerFunc(param, zeroFPS, param);
+}
+
 static void drawScene() {
+	++fps_counter;
+
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	mn::Camera &camera = *mn::Camera::getDefaultCamera();
+	const mn::Camera &camera = *mn::Camera::getDefaultCamera();
+	const mn::Vector &eye = camera.getEye();
 
 	{
 		pushMatrix pm;
-		camera.doLookAt();
 
 		glEnable(GL_LIGHTING);
 		glEnable(GL_LIGHT0);
+		glEnable(GL_LIGHT1);
 
-		GLfloat lightColor0[] = {1.0f, 1.0f, 1.0f, 1.0f};
-		GLfloat lightPos0[] = {0, 0, 0, 1.0f};
+		camera.doLookAt();
+
+		static const GLfloat lightColor0[] = {1.0f, 1.0f, 1.0f, 1.0f};
+		static const GLfloat lightPos0[] = {0, 0, 0, 1.0f};
 		glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
 		glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
 
 		sun->draw(mn::Camera::getTicks());
 
+		glDisable(GL_LIGHT1);
 		glDisable(GL_LIGHT0);
 		glDisable(GL_LIGHTING);
 	}
@@ -73,11 +88,20 @@ static void drawScene() {
 	{
 		pushMatrix pm;
 
-		glTranslatef(-0.8, 0.8, -2);
+		glTranslatef(0, 0, -2);
+
+		glColor3f(0, 0.7, 0);
+		glBegin(GL_LINES);
+		glVertex3f(-0.01,  0.01, 0); glVertex3f(-0.02,  0.02, 0);
+		glVertex3f(-0.01, -0.01, 0); glVertex3f(-0.02, -0.02, 0);
+		glVertex3f( 0.01, -0.01, 0); glVertex3f( 0.02, -0.02, 0);
+		glVertex3f( 0.01,  0.01, 0); glVertex3f( 0.02,  0.02, 0);
+		glEnd();
+
+		glTranslatef(-0.8, 0.8, 0);
 		glScalef(0.03, 0.03, 0.03);
 		char buffer[1024];
-		const mn::Vector &eye = camera.getEye();
-		sprintf(buffer, "position = (%3.3f, %3.3f, %3.3f)\nrotation = (%3.3f, %3.3f, %3.3f)", eye.x, eye.y, eye.z, camera.getRotX() * (180/M_PI), camera.getRotY() * (180/M_PI), 0.0);
+		sprintf(buffer, "position = (%3.3f, %3.3f, %3.3f)\nrotation = (%3.3f, %3.3f, %3.3f)\nfps = %3.1f", eye.x, eye.y, eye.z, camera.getRotX() * (180/M_PI), camera.getRotY() * (180/M_PI), 0.0, fps);
 		glColor3f(1, 1, 1);
 		t3d::draw2D(std::string(buffer), -1, -1);
 	}
@@ -126,27 +150,31 @@ int main(int argc, char** argv) {
 	const float distanceFactor = 149597870691e-10;  /* AU */
 	const float sizeFactor = 6371000e-8; /* Earth's mean radius */
 	const float omegaFactor = 0.1;
+
 	sun = new mn::Sphere(0, /*109*/ 20 * sizeFactor, 0, mn::Color(1, 1, 0),
 	                     "Sun");
+	mn::Sphere *top = sun;
 #define P(distance, radius, period, r, g, b, name) \
-	sun->pushSatelite(*new mn::Sphere(distance * distanceFactor, \
+	top->pushSatelite(*new mn::Sphere(distance * distanceFactor,	\
 	                                  radius * sizeFactor, \
 	                                  omegaFactor/period, \
 	                                  mn::Color(r, g, b), name));
-	P(88.0, 0.14, 12050, 1.0, 1.0, 1.0, "90377 Sedna");
-	P(67.7, 0.19, 557.0, 1.0, 1.0, 1.0, "136199 Eris");
-	P(39.5, 0.19, 248.1, 1.0, 1.0, 1.0, "134340 Pluto");
-	P(30.1, 3.88, 164.9, 1.0, 1.0, 1.0, "Neptune");
-	P(19.2, 4.00, 84.07, 1.0, 1.0, 1.0, "Uranus");
-	P(9.58, 9.45, 29.45, 1.0, 1.0, 1.0, "Saturn");
-	P(5.20, 11.2, 11.87, 1.0, 1.0, 1.0, "Jupiter");
-	P(3.14, 0.03, 5.557, 1.0, 1.0, 1.0, "10 Hygiea");
-	P(2.77, 0.07, 4.600, 1.0, 1.0, 1.0, "1 Ceres");
-	P(2.36, 0.04, 3.629, 1.0, 1.0, 1.0, "4 Vesta");
-	P(1.50, 0.53, 1.881, 1.0, 0.0, 0.0, "Mars");
-	P(1.00, 1.00, 1.000, 0.0, 1.0, 1.0, "Earth");
-	P(0.73, 0.95, 0.615, 1.0, 1.0, 1.0, "Venus");
 	P(0.38, 0.38, 0.241, 1.0, 1.0, 1.0, "Mercury");
+	P(0.73, 0.95, 0.615, 1.0, 1.0, 1.0, "Venus");
+	top = &P(1.00, 1.00, 1.000, 0.0, 1.0, 1.0, "Earth");
+	P(0.01, 0.01, 0.074, 0.8, 0.8, 0.8, "Moon");
+	top = sun;
+	P(1.50, 0.53, 1.881, 1.0, 0.0, 0.0, "Mars");
+	P(2.36, 0.04, 3.629, 1.0, 1.0, 1.0, "4 Vesta");
+	P(2.77, 0.07, 4.600, 1.0, 1.0, 1.0, "1 Ceres");
+	P(3.14, 0.03, 5.557, 1.0, 1.0, 1.0, "10 Hygiea");
+	P(5.20, 11.2, 11.87, 1.0, 1.0, 1.0, "Jupiter");
+	P(9.58, 9.45, 29.45, 1.0, 1.0, 1.0, "Saturn");
+	P(19.2, 4.00, 84.07, 1.0, 1.0, 1.0, "Uranus");
+	P(30.1, 3.88, 164.9, 1.0, 1.0, 1.0, "Neptune");
+	P(39.5, 0.19, 248.1, 1.0, 1.0, 1.0, "134340 Pluto");
+	P(67.7, 0.19, 557.0, 1.0, 1.0, 1.0, "136199 Eris");
+	P(88.0, 0.14, 12050, 1.0, 1.0, 1.0, "90377 Sedna");
 #undef P
 
 	mn::Camera camera;
@@ -154,14 +182,23 @@ int main(int argc, char** argv) {
 	mn::Camera::setSize(w, h);
 
 	mn::Camera::keyMovementFactor   *=  3;
-	mn::Camera::keyRunFactor        *=  3;
 	mn::Camera::mouseMovementFactor *=  3;
-	mn::Camera::mouseRunFactor      *=  3;
 
 	mn::Camera::registerHandlers();
 	mn::Camera::setResizeFunc(handleResize);
 	mn::Camera::setKeyboardFunc(handleKeyboard);
 	glutDisplayFunc(drawScene);
+
+
+	static const GLfloat lightColor1[] = {0.0f, 1.0f, 1.0f, 1.0f};
+	static const GLfloat lightPos1[] = { 0, 0, 0, 0 };
+	static const GLfloat lightDirection1[] = { 0, 0, 0, -1 };
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, lightColor1);
+	glLightfv(GL_LIGHT1, GL_POSITION, lightPos1);
+	glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, lightDirection1);
+	glLightf (GL_LIGHT1, GL_SPOT_CUTOFF, 15.f);
+
+	glutTimerFunc(1000, zeroFPS, 1000);
 
 	glutMainLoop();
 	return 0;
