8e9c518af3f2fc2d7ce8cd85e0c30cdfccbfd0af
Michal Nazarewicz <mina86/AT/mina86.com>
2009-01-17 22:19:45 +0100

    Added licensing information.

--- src/common/text3d.cpp
+++ src/common/text3d.cpp
@@ -1,3 +1,20 @@
+/*
+ * src/common/text3d.cpp
+ * Copyright 2009 by Michal Nazarewicz (mina86/AT/mina86/DOT/com)
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /* Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
