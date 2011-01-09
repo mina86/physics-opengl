06cb0e80543cb8da61f796209902cf01bd9e781d
Maciej Andrzej Świętochowski <m@swietochowski.eu>
2011-01-09 21:43:54 +0100

    New settings in evolutinary solver config.

--- src/graph/solver/evo/evoconfig.cpp
+++ src/graph/solver/evo/evoconfig.cpp
@@ -7,7 +7,14 @@ namespace solver {
 namespace evo {
 
 Data::Data()
-	: populationSize("Population size", 1, 1000, 20)
+	: populationSize("Population size", 1, 1000, 20),
+	selectionType("Selection type", selectionTypes, Trivial),
+	crossoverType("Crossover type", crossoverTypes, NoCrossover),
+	successionType("Succession type", successionTypes, EliteOfUnion),
+	selectionInteger1("Selection: k-param", 1, 1000, 2),
+	mutationEnabled("Enable mutation (normal distribtuion)?", true),
+	mutationMu("Mutation mean value", -10, 10, 0),
+	mutationSigmasquared("Mutation variance", -10, 10, 0)
 {
 	init();
 }
--- src/graph/solver/evo/evoconfig.hpp
+++ src/graph/solver/evo/evoconfig.hpp
@@ -9,12 +9,37 @@ namespace solver {
 
 namespace evo {
 
+enum SelectionType { Trivial, RandomUniform, Proportional, Tournament };
+const char * const selectionTypes[] = {
+	"Trivial", "Random uniform", "Proportional", "Tournament(k)"
+};
+
+enum CrossoverType { NoCrossover, Arithmetic, Interchange };
+const char * const crossoverTypes[] = {
+	"None", "Arithmetic", "Interchange"
+};
+
+enum SuccessionType { Straight, EliteOfUnion /*, EliteSumOf*/ };
+const char * const successionTypes[] = {
+	"Straight", "Elite of union" /*, "Union of elite" */
+};
+
 struct Data;
 
 typedef ui::Config<Data> Config;
 
 struct Data : public ui::cfg::Data {
 	ui::cfg::Integer populationSize;
+	ui::cfg::List selectionType;
+	ui::cfg::List crossoverType;
+	ui::cfg::List successionType;
+
+	ui::cfg::Integer selectionInteger1;
+
+	// Mutation X ~ N(mu, sigma^2)
+	ui::cfg::Bool mutationEnabled;
+	ui::cfg::Real mutationMu;
+	ui::cfg::Real mutationSigmasquared;
 
 	virtual struct iterator items() const;
 
