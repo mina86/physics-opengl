aacc6c4fb7ed3f5e8209de79c2d40c48a3046e57
Maciej Andrzej Świętochowski <m@swietochowski.eu>
2011-01-16 21:08:27 +0100

    Added Close button to AutoSettingsDialog.

--- src/ui/dialogs/autosettingsdialog.cpp
+++ src/ui/dialogs/autosettingsdialog.cpp
@@ -1,20 +1,34 @@
 #include "autosettingsdialog.hpp"
 
 #include <QFormLayout>
+#include <QVBoxLayout>
+#include <QDialogButtonBox>
+#include <QPushButton>
 
 namespace ui {
 
 AutoSettingsDialog::AutoSettingsDialog(cfg::Data *data, QWidget *parent, Qt::WindowFlags f) :
 	QDialog(parent, f)
 {
-	QFormLayout *layout = new QFormLayout(this);
+	QVBoxLayout *dialogLayout = new QVBoxLayout(this);
 
+	QWidget *optionsWidget = new QWidget(this);
+	QFormLayout *layout = new QFormLayout(optionsWidget);
 	for (cfg::Data::iterator it = data->items(); it; ++it) {
 		cfg::Value *valueItem = &(it(data));
-		layout->addRow(valueItem->name, valueItem->makeControlWidget(this));
+		layout->addRow(valueItem->name, valueItem->makeControlWidget(optionsWidget));
 	}
+	optionsWidget->setLayout(layout);
+	dialogLayout->addWidget(optionsWidget);
 
-	setLayout(layout);
+	QPushButton *closeButton = new QPushButton(tr("Clo&se"), this);
+	closeButton->setDefault(true);
+	connect(closeButton, SIGNAL(clicked()), this, SLOT(close()));
+	QDialogButtonBox *bbox = new QDialogButtonBox(Qt::Horizontal, this);
+	bbox->addButton(closeButton, QDialogButtonBox::YesRole);
+	dialogLayout->addWidget(bbox);
+
+	setLayout(dialogLayout);
 }
 
 }
