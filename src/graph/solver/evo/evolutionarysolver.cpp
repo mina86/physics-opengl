8ca8935fed55e031374980b4994599e0239f1a95
Maciej Andrzej Świętochowski <m@swietochowski.eu>
2011-01-16 19:31:36 +0100

    Removed unused EvolutionarySolverPlayer.

--- src/graph/solver/evo/evolutionarysolver.cpp
+++ src/graph/solver/evo/evolutionarysolver.cpp
@@ -17,7 +17,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include "evolutionarysolver.hpp"
-#include "evolutionarysolverplayer.hpp"
 #include "../../../lib/rand.hpp"
 #include "../../../lib/auto-array.hpp"
 #include "../../vector-rand.hpp"
