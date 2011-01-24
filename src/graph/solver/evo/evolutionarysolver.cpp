26f103ab7bede9f986f5453663ccd81d0ae4474e
Michal Nazarewicz <mina86@mina86.com>
2011-01-24 20:59:28 +0100

    evo: various tweaks
    The Score class has been removed and instead of "valid" flag
    a quiet NaN is used to distinguish invalid scores.  Also, instead
    of storing graph and it's score in a std::pair<> an Individual
    class has been created with definition in .cpp file.
    
    Definition of the Compare class has also been moved to .cpp file.
    
    The evaluate() method has been moved outside from the Compare
    class and defined as global function in the cpp file.

--- src/graph/solver/evo/evolutionarysolver.cpp
+++ src/graph/solver/evo/evolutionarysolver.cpp
@@ -28,11 +28,65 @@ namespace graph {
 
 namespace solver {
 
-EvolutionarySolver::EvolutionarySolver(Scene &scene) : AbstractSolver(scene) {
-	iterationCount = 0;
-	//TODO: adjust population size when populationSize changed
-	individual_t seed(static_cast<Graph &>(scene), Score());
-	population = population_ptr(new population_t(config->populationSize, seed));
+
+static double evaluate(const Graph &g, const evo::Data &data);
+
+
+struct Individual {
+	Individual(const Graph &theGraph, double theScore)
+		: graph_(theGraph), score_(theScore) { }
+
+	Individual(const Graph &theGraph, const evo::Data &data)
+		: graph_(theGraph), score_(evaluate(theGraph, data)) { }
+
+	Graph       &graph() {
+		score_ = std::numeric_limits<double>::quiet_NaN();
+		return graph_;
+	}
+	const Graph &graph() const { return graph_; }
+	const Graph &constgraph() { return graph_; }
+
+	double score(const evo::Data &data) const {
+		if (isnan(score_)) {
+			score_ = evaluate(graph_, data);
+		}
+		return score_;
+	}
+
+private:
+	Individual();
+
+	Graph graph_;
+	mutable double score_;
+};
+
+
+namespace {
+
+struct Compare {
+	Compare(const evo::Data &theData) : data(theData) { }
+
+	double operator()(const Individual &ind) { return ind.score(data); }
+
+	bool operator()(const Individual &a, const Individual &b) {
+		return a.score(data) < b.score(data);
+	}
+
+	bool operator()(const Individual *a, const Individual *b) {
+		return (*this)(*a, *b);
+	}
+
+private:
+	const evo::Data &data;
+};
+
+}
+
+
+EvolutionarySolver::EvolutionarySolver(Scene &scene)
+	: AbstractSolver(scene), iterationCount(0) {
+	Individual seed(scene, *config);
+	population.reset(new Population(config->populationSize, Individual(scene, *config)));
 }
 
 QWidget* EvolutionarySolver::createPlayerWidget(QWidget *theParent)
@@ -43,95 +97,95 @@ QWidget* EvolutionarySolver::createPlayerWidget(QWidget *theParent)
 }
 
 
-EvolutionarySolver::population_ptr EvolutionarySolver::reproduce(const population_t * const population)
+EvolutionarySolver::population_ptr EvolutionarySolver::reproduce(const Population *population)
 {
-	population_t::size_type newPopulationSize = config->populationSize;
+	Population::size_type newPopulationSize = config->populationSize;
 	switch (config->selectionType) {
 	case evo::Trivial:
-		{
-			population_ptr offsprings(new population_t());
-			while (offsprings->size() < newPopulationSize) {
-				for (population_t::const_iterator i = population->begin();
-				i != population->end() && offsprings->size() < newPopulationSize;
-				++i) {
-					offsprings->push_back(*i);
-				}
+	{
+		population_ptr offsprings(new Population());
+		while (offsprings->size() < newPopulationSize) {
+			for (Population::const_iterator i = population->begin();
+				 i != population->end() && offsprings->size() < newPopulationSize;
+				 ++i) {
+				offsprings->push_back(*i);
 			}
-			return offsprings;
 		}
-		break;
+		return offsprings;
+	}
+	break;
 	case evo::RandomUniform:
-		{
-			population_ptr offsprings(new population_t());
-			while (offsprings->size() < newPopulationSize) {
-				population_t::size_type idx;
-				idx = lib::rnd<population_t::size_type>(population->size()-1);
-				offsprings->push_back((*population)[idx]);
-			}
-			return offsprings;
+	{
+		population_ptr offsprings(new Population());
+		while (offsprings->size() < newPopulationSize) {
+			Population::size_type idx;
+			idx = lib::rnd<Population::size_type>(population->size()-1);
+			offsprings->push_back((*population)[idx]);
 		}
+		return offsprings;
+	}
 	case evo::Proportional:
-		{
-			population_ptr offsprings(new population_t());
-			population_t::size_type s = population->size();
-			lib::auto_array<double> a_distribution(new double[s+1]);
-			double *distribution = a_distribution.get(); //cumulative distribution
-			double sum = 0;
-			population_t::const_iterator i = population->begin();
-			population_t::size_type j = 1;
-			Compare evaluator(*config);
-			while (i != population->end()) {
-				double eval = evaluator(*i);
-				distribution[j] = eval;
-				sum += eval;
-				++i;
-				++j;
-			}
-			distribution[0] = 0;
-			for (j=1; j <= s; ++j) {
-				distribution[j] /= sum;
-				distribution[j] += distribution[j-1];
-			}
-			distribution[s-1] = 1;
+	{
+		population_ptr offsprings(new Population());
+		Population::size_type s = population->size();
+		lib::auto_array<double> a_distribution(new double[s+1]);
+		double *distribution = a_distribution.get(); //cumulative distribution
+		double sum = 0;
+		Population::const_iterator i = population->begin();
+		Population::size_type j = 1;
+		Compare evaluator(*config);
+		while (i != population->end()) {
+			double eval = evaluator(*i);
+			distribution[j] = eval;
+			sum += eval;
+			++i;
+			++j;
+		}
+		distribution[0] = 0;
+		for (j=1; j <= s; ++j) {
+			distribution[j] /= sum;
+			distribution[j] += distribution[j-1];
+		}
+		distribution[s-1] = 1;
 
-			for (j=0; j < newPopulationSize; ++j) {
-				double r = lib::rndp<double>(1);
-				//TODO: optimization with iterative binary search (for huge populations)
-				for (unsigned k = 0; k < s; ++k) {
-					if (distribution[k] < r && r <= distribution[k+1]) {
-						offsprings->push_back((*population)[k]);
-						break;
-					}
+		for (j=0; j < newPopulationSize; ++j) {
+			double r = lib::rndp<double>(1);
+			//TODO: optimization with iterative binary search (for huge populations)
+			for (unsigned k = 0; k < s; ++k) {
+				if (distribution[k] < r && r <= distribution[k+1]) {
+					offsprings->push_back((*population)[k]);
+					break;
 				}
 			}
-			return offsprings;
 		}
+		return offsprings;
+	}
 	case evo::Tournament:
-		{
-			population_ptr offsprings(new population_t());
+	{
+		population_ptr offsprings(new Population());
 
-			//prepare pointers vector
-			lib::auto_array<const individual_t *> a_indivptrs(new const individual_t*[population->size()]);
-			const individual_t **indivptrs = a_indivptrs.get();
-			for (population_t::size_type i = 0; i < population->size(); ++i) {
-				indivptrs[i] = &((*population)[i]);
-			}
-
-			//population->size()-times do k-element tournament
-			//pick tournament members by swapping them to beginning of pointers vector
-			//resolve tournament by sorting front of that vector on evaluation score
-			for (population_t::size_type s = 0; s < newPopulationSize; ++s) {
-				for (ui::cfg::Integer::value_type k = 0; k < config->selectionInteger1; ++k) {
-					population_t::size_type r = lib::rndp<population_t::size_type>(population->size()-k);
-					std::swap(indivptrs[k], indivptrs[r+k]);
-				}
-				std::sort(indivptrs, indivptrs + config->selectionInteger1, Compare(*config));
-				offsprings->push_back(individual_t(*(indivptrs[0])));
-			}
-			return offsprings;
+		//prepare pointers vector
+		lib::auto_array<const Individual *> a_indivptrs(new const Individual*[population->size()]);
+		const Individual **indivptrs = a_indivptrs.get();
+		for (Population::size_type i = 0; i < population->size(); ++i) {
+			indivptrs[i] = &((*population)[i]);
 		}
+
+		//population->size()-times do k-element tournament
+		//pick tournament members by swapping them to beginning of pointers vector
+		//resolve tournament by sorting front of that vector on evaluation score
+		for (Population::size_type s = 0; s < newPopulationSize; ++s) {
+			for (ui::cfg::Integer::value_type k = 0; k < config->selectionInteger1; ++k) {
+				Population::size_type r = lib::rndp<Population::size_type>(population->size()-k);
+				std::swap(indivptrs[k], indivptrs[r+k]);
+			}
+			std::sort(indivptrs, indivptrs + config->selectionInteger1, Compare(*config));
+			offsprings->push_back(Individual(*(indivptrs[0])));
+		}
+		return offsprings;
+	}
 	default:
-		return population_ptr(new population_t(*population));
+		return population_ptr(new Population(*population));
 	}
 }
 
@@ -139,72 +193,72 @@ EvolutionarySolver::population_ptr EvolutionarySolver::genetic(population_ptr re
 {
 	if (config->crossoverProbability > 0) {
 		std::random_shuffle(reproduced->begin(), reproduced->end(), lib::rnd<long>);
-		population_t::size_type numberOfPairs = reproduced->size() / 2;
+		Population::size_type numberOfPairs = reproduced->size() / 2;
 		switch (config->crossoverType) {
 		case evo::Mean:
-			{
-				float middlepoint = 0.5;
-				float middlepoint2 = 1 - middlepoint;
-				for (population_t::size_type i = 0; i < numberOfPairs; ++i) {
-					if (lib::rndp<float>(1.0) <= config->crossoverProbability) {
-						Graph &g1 = graph((*reproduced)[2*i  ]);
-						const Graph &g2 = constgraph((*reproduced)[2*i+1]);
-						Graph::nodes_iterator ni1 = g1.nodes_begin();
-						Graph::const_nodes_iterator ni2 = g2.nodes_begin();
-						while (ni1 != g1.nodes_end()) {
-							ni1->set(ni1->x()*middlepoint + ni2->x()*middlepoint2,
-									 ni1->y()*middlepoint + ni2->y()*middlepoint2,
-									 ni1->z()*middlepoint + ni2->z()*middlepoint2);
-							++ni1;
-							++ni2;
-						}
+		{
+			float middlepoint = 0.5;
+			float middlepoint2 = 1 - middlepoint;
+			for (Population::size_type i = 0; i < numberOfPairs; ++i) {
+				if (lib::rndp<float>(1.0) <= config->crossoverProbability) {
+					Graph &g1 = (*reproduced)[2*i  ].graph();
+					const Graph &g2 = (*reproduced)[2*i+1].constgraph();
+					Graph::nodes_iterator ni1 = g1.nodes_begin();
+					Graph::const_nodes_iterator ni2 = g2.nodes_begin();
+					while (ni1 != g1.nodes_end()) {
+						ni1->set(ni1->x()*middlepoint + ni2->x()*middlepoint2,
+								 ni1->y()*middlepoint + ni2->y()*middlepoint2,
+								 ni1->z()*middlepoint + ni2->z()*middlepoint2);
+						++ni1;
+						++ni2;
 					}
 				}
 			}
-			break;
+		}
+		break;
 		case evo::Arithmetic:
-			{
-				float middlepoint = lib::rnd<float>(1.0);
-				float middlepoint2 = 1 - middlepoint;
-				for (population_t::size_type i = 0; i < numberOfPairs; ++i) {
-					if (lib::rndp<float>(1.0) <= config->crossoverProbability) {
-						Graph &g1 = graph((*reproduced)[2*i  ]);
-						const Graph &g2 = constgraph((*reproduced)[2*i+1]);
-						Graph::nodes_iterator ni1 = g1.nodes_begin();
-						Graph::const_nodes_iterator ni2 = g2.nodes_begin();
-						while (ni1 != g1.nodes_end()) {
-							ni1->set(ni1->x()*middlepoint + ni2->x()*middlepoint2,
-									 ni1->y()*middlepoint + ni2->y()*middlepoint2,
-									 ni1->z()*middlepoint + ni2->z()*middlepoint2);
-							++ni1;
-							++ni2;
-						}
+		{
+			float middlepoint = lib::rnd<float>(1.0);
+			float middlepoint2 = 1 - middlepoint;
+			for (Population::size_type i = 0; i < numberOfPairs; ++i) {
+				if (lib::rndp<float>(1.0) <= config->crossoverProbability) {
+					Graph &g1 = (*reproduced)[2*i  ].graph();
+					const Graph &g2 = (*reproduced)[2*i+1].constgraph();
+					Graph::nodes_iterator ni1 = g1.nodes_begin();
+					Graph::const_nodes_iterator ni2 = g2.nodes_begin();
+					while (ni1 != g1.nodes_end()) {
+						ni1->set(ni1->x()*middlepoint + ni2->x()*middlepoint2,
+								 ni1->y()*middlepoint + ni2->y()*middlepoint2,
+								 ni1->z()*middlepoint + ni2->z()*middlepoint2);
+						++ni1;
+						++ni2;
 					}
 				}
 			}
-			break;
+		}
+		break;
 		case evo::Interchange:
-			{
-				for (population_t::size_type i = 0; i < numberOfPairs; ++i) {
-					if (lib::rndp<float>(1.0) <= config->crossoverProbability) {
-						Graph &g1 = graph((*reproduced)[2*i  ]);
-						Graph &g2 = graph((*reproduced)[2*i+1]);
-						Graph::nodes_iterator ni1 = g1.nodes_begin();
-						Graph::nodes_iterator ni2 = g2.nodes_begin();
-						unsigned j=0;
-						unsigned breakingpoint = g1.nodes() * lib::rndp(1.0);
-						while (j < breakingpoint) {
-							Graph::node_type tmp(*ni1);
-							(*ni1) = (*ni2);
-							(*ni2) = tmp;
-							++ni1;
-							++ni2;
-							++j;
-						}
+		{
+			for (Population::size_type i = 0; i < numberOfPairs; ++i) {
+				if (lib::rndp<float>(1.0) <= config->crossoverProbability) {
+					Graph &g1 = (*reproduced)[2*i  ].graph();
+					Graph &g2 = (*reproduced)[2*i+1].graph();
+					Graph::nodes_iterator ni1 = g1.nodes_begin();
+					Graph::nodes_iterator ni2 = g2.nodes_begin();
+					unsigned j=0;
+					unsigned breakingpoint = g1.nodes() * lib::rndp(1.0);
+					while (j < breakingpoint) {
+						Graph::node_type tmp(*ni1);
+						(*ni1) = (*ni2);
+						(*ni2) = tmp;
+						++ni1;
+						++ni2;
+						++j;
 					}
 				}
 			}
-			break;
+		}
+		break;
 		default:
 			/* no crossover */
 			break;
@@ -213,11 +267,12 @@ EvolutionarySolver::population_ptr EvolutionarySolver::genetic(population_ptr re
 
 	//Mutation
 	if (config->mutationProbability > 0) {
-		for (population_t::iterator i = reproduced->begin(); i != reproduced->end(); ++i)
+		for (Population::iterator i = reproduced->begin(); i != reproduced->end(); ++i)
 		{
 			float r = lib::rndp<float>(1.0);
 			if (r <= config->mutationProbability) {
-				for (Graph::nodes_iterator j = graph(*i).nodes_begin(); j != graph(*i).nodes_end(); ++j) {
+				for (Graph::nodes_iterator j = i->graph().nodes_begin(),
+						 end = i->graph().nodes_end(); j != end; ++j) {
 					graph::addRandNormal(*j, config->mutationSigma);
 				}
 			}
@@ -233,56 +288,56 @@ EvolutionarySolver::population_ptr EvolutionarySolver::succession(population_ptr
 		std::sort(offsprings->begin(), offsprings->end(), Compare(*config));
 		return offsprings;
 	case evo::EliteOfUnion:
+	{
+		population_ptr newpopulation(new Population());
+		std::sort(population->begin(), population->end(), Compare(*config));
+		std::sort(offsprings->begin(), offsprings->end(), Compare(*config));
+
+		Population::const_iterator i = population->begin(), j = offsprings->begin();
+
+		Compare evaluator(*config);
+		while (i != population->end() && j != offsprings->end() && newpopulation->size() < population->size())
 		{
-			population_ptr newpopulation(new population_t());
-			std::sort(population->begin(), population->end(), Compare(*config));
-			std::sort(offsprings->begin(), offsprings->end(), Compare(*config));
-
-			population_t::const_iterator i = population->begin(), j = offsprings->begin();
-
-			Compare evaluator(*config);
-			while (i != population->end() && j != offsprings->end() && newpopulation->size() < population->size())
+			if (evaluator(*i) <= evaluator(*j))
 			{
-				if (evaluator(*i) <= evaluator(*j))
-				{
-					newpopulation->push_back(*i);
-					++i;
-				}
-				else
-				{
-					newpopulation->push_back(*j);
-					++j;
-				}
+				newpopulation->push_back(*i);
+				++i;
 			}
-
-			while (i != population->end() && newpopulation->size() < population->size())
-				newpopulation->push_back(*(i++));
-			while (j != offsprings->end() && newpopulation->size() < population->size())
-				newpopulation->push_back(*(j++));
-
-			return newpopulation;
+			else
+			{
+				newpopulation->push_back(*j);
+				++j;
+			}
 		}
+
+		while (i != population->end() && newpopulation->size() < population->size())
+			newpopulation->push_back(*(i++));
+		while (j != offsprings->end() && newpopulation->size() < population->size())
+			newpopulation->push_back(*(j++));
+
+		return newpopulation;
+	}
 	case evo::EliteSumOf:
+	{
+		population_ptr newpopulation(new Population());
+		std::sort(population->begin(), population->end(), Compare(*config));
+		std::sort(offsprings->begin(), offsprings->end(), Compare(*config));
+
+		Population::const_iterator i[2];
+		i[0] = population->begin();
+		i[1] = offsprings->begin();
+		unsigned j = 0;
+
+		while (i[0] != population->end() && i[1] != offsprings->end() && newpopulation->size() < population->size())
 		{
-			population_ptr newpopulation(new population_t());
-			std::sort(population->begin(), population->end(), Compare(*config));
-			std::sort(offsprings->begin(), offsprings->end(), Compare(*config));
-
-			population_t::const_iterator i[2];
-			i[0] = population->begin();
-			i[1] = offsprings->begin();
-			unsigned j = 0;
-
-			while (i[0] != population->end() && i[1] != offsprings->end() && newpopulation->size() < population->size())
-			{
-				newpopulation->push_back(*(i[j]));
-				++(i[j]);
-				j = 1 - j;
-			}
-
-			std::sort(newpopulation->begin(), newpopulation->end(), Compare(*config));
-			return newpopulation;
+			newpopulation->push_back(*(i[j]));
+			++(i[j]);
+			j = 1 - j;
 		}
+
+		std::sort(newpopulation->begin(), newpopulation->end(), Compare(*config));
+		return newpopulation;
+	}
 	default:
 		/* no succession -- should never happen */
 		return population;
@@ -301,12 +356,12 @@ void EvolutionarySolver::playNextFrame(unsigned iterations)
 
 void EvolutionarySolver::updateScene()
 {
-	scene.set(constgraph(population->front()));
+	scene.set(population->front().constgraph());
 	emit graphChanged();
 }
 
 
-double Compare::evaluate(const Graph &g) {
+static double evaluate(const Graph &g, const evo::Data &data) {
 	double result = 0.0;
 	double exclusiveRadius = 1.0;
 
@@ -315,7 +370,7 @@ double Compare::evaluate(const Graph &g) {
 
 	for (i = g.nodes_begin(); i != g.nodes_end(); ++i)
 	{
-		ki = (i-g.nodes_begin());
+		ki = i - g.nodes_begin();
 		j = i;
 		++j;
 
--- src/graph/solver/evo/evolutionarysolver.hpp
+++ src/graph/solver/evo/evolutionarysolver.hpp
@@ -21,39 +21,32 @@
 
 #include "../abstractsolver.hpp"
 #include "../../../gl/vector.hpp"
+#include "../../../lib/isnan.hpp"
 
 #include "evoconfig.hpp"
 
 #include <vector>
+#include <limits>
 
 namespace graph {
 
 namespace solver {
+
+struct Individual;
 
 struct EvolutionarySolver : public AbstractSolver {
 	EvolutionarySolver(Scene &scene);
 	virtual QWidget* createPlayerWidget(QWidget *parent);
 	ui::cfg::Data* getConfigData() { return &(*config); }
 
-	struct Score {
-		Score() : valid(false) { }
-		mutable bool valid;
-		mutable double score;
-	};
-
-	typedef std::pair<Graph, Score> individual_t;
-	typedef std::vector<individual_t> population_t;
-	typedef std::auto_ptr<population_t> population_ptr;
+	typedef std::vector<Individual> Population;
+	typedef std::auto_ptr<Population> population_ptr;
 
 protected:
-	static Graph&       graph(individual_t &i)            { i.second.valid = false; return i.first; }
-	static const Graph& graph(const individual_t &i)      { return i.first; }
-	static const Graph& constgraph(const individual_t &i) { return i.first; }
-
 	int iterationCount;
 	void updateScene();
 	population_ptr population;
-	population_ptr reproduce(const population_t * const population);
+	population_ptr reproduce(const Population *population);
 	population_ptr genetic(population_ptr reproduced);
 	population_ptr succession(population_ptr population, population_ptr offsprings);
 
@@ -64,23 +57,6 @@ private:
 	evo::Config config;
 
 	Q_OBJECT
-};
-
-struct Compare {
-	Compare(const evo::Data &theData) : data(theData) { }
-	double operator()(const EvolutionarySolver::individual_t &ind) { return evaluate(ind); }
-	bool operator()(const EvolutionarySolver::individual_t &first, const EvolutionarySolver::individual_t &second) { return evaluate(first) < evaluate(second); }
-	bool operator()(const EvolutionarySolver::individual_t *first, const EvolutionarySolver::individual_t *second) { return evaluate(*first) < evaluate(*second); }
-private:
-	double evaluate(const EvolutionarySolver::individual_t &i) {
-		if (!i.second.valid) {
-			i.second.score = evaluate(i.first);
-			i.second.valid = true;
-		}
-		return i.second.score;
-	}
-	double evaluate(const Graph &g);
-	const evo::Data &data;
 };
 
 }
